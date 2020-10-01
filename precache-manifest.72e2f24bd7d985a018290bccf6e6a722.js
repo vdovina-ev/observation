d287d17cee2f9f5dc0b6b3b147b2f353b747a79e
unknown <vdovina.ev.1@gmail.com>
2020-10-01 23:32:57 +0300

    Updates

---
+++ precache-manifest.72e2f24bd7d985a018290bccf6e6a722.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a56166feeacd7b87d9649bd4cf120a7",
+    "url": "/observation/index.html"
+  },
+  {
+    "revision": "f096c302a0b67b0c20b6",
+    "url": "/observation/static/css/2.e0686c58.chunk.css"
+  },
+  {
+    "revision": "d305351694257bb5ab02",
+    "url": "/observation/static/css/main.7fe5cd09.chunk.css"
+  },
+  {
+    "revision": "f096c302a0b67b0c20b6",
+    "url": "/observation/static/js/2.eece989f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/observation/static/js/2.eece989f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d305351694257bb5ab02",
+    "url": "/observation/static/js/main.5f70f84e.chunk.js"
+  },
+  {
+    "revision": "18e1ee9101124330a619",
+    "url": "/observation/static/js/runtime-main.36702969.js"
+  },
+  {
+    "revision": "0662d9de8af86fb66060a046fbafe1b5",
+    "url": "/observation/static/media/GC_logo.0662d9de.svg"
+  },
+  {
+    "revision": "2f311a05a79fb86cee6148d658aa8336",
+    "url": "/observation/static/media/ITN_logo.2f311a05.svg"
+  },
+  {
+    "revision": "43f9dac62ffb9209e823b91b7504c5ec",
+    "url": "/observation/static/media/energetick.43f9dac6.jpg"
+  },
+  {
+    "revision": "fb084e1e416e340515649cb48eb04f6e",
+    "url": "/observation/static/media/energy.fb084e1e.jpg"
+  },
+  {
+    "revision": "3573d221b566ebfaf5f95a160435af71",
+    "url": "/observation/static/media/hero-bg.3573d221.jpg"
+  },
+  {
+    "revision": "5ec44869469e2f3c9db3551c44662f88",
+    "url": "/observation/static/media/industry.5ec44869.jpg"
+  },
+  {
+    "revision": "341a487c2bb1cbed9179faa73d89b343",
+    "url": "/observation/static/media/shid_logo.341a487c.svg"
+  },
+  {
+    "revision": "f8f67844654cd9fc78dd57834ecfed6c",
+    "url": "/observation/static/media/sity-planing.f8f67844.jpg"
+  },
+  {
+    "revision": "7e1db0da3d25263be6534e000b29d2a8",
+    "url": "/observation/static/media/slide-1.7e1db0da.jpg"
+  },
+  {
+    "revision": "12a25e248f105a327021b69336c5b71b",
+    "url": "/observation/static/media/slide-2.12a25e24.jpg"
+  },
+  {
+    "revision": "638ec2275a294137e028e6bce58ad768",
+    "url": "/observation/static/media/slide-3.638ec227.jpg"
+  },
+  {
+    "revision": "8a21e8b7fc0908ab482cc0bb0c98138a",
+    "url": "/observation/static/media/slide-4.8a21e8b7.jpg"
+  },
+  {
+    "revision": "bb9f49bba9c380e18e77de4c1ed82bf6",
+    "url": "/observation/static/media/slide-5.bb9f49bb.jpg"
+  },
+  {
+    "revision": "65643b05446e10934c8778999860e9de",
+    "url": "/observation/static/media/transport.65643b05.jpg"
+  },
+  {
+    "revision": "a8b6caeec11d336276f5ed05a63fc938",
+    "url": "/observation/static/media/utilisation.a8b6caee.jpg"
+  }
+]);
